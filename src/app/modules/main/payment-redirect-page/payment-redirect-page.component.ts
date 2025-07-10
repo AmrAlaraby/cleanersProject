a279ab502a06b16e79794e2a074b308ab76fc5e3
AmrAlaraby <amr2amr3@gmail.com>
2025-07-10 18:20:42 +0300

    Refactor code structure for improved readability and maintainability

--- src/app/modules/main/payment-redirect-page/payment-redirect-page.component.ts
+++ src/app/modules/main/payment-redirect-page/payment-redirect-page.component.ts
@@ -9,6 +9,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class PaymentRedirectPageComponent implements OnInit {
 isError: boolean = false;
   message: string = '';
+  orderId: string | null = null;
 
   constructor(private route: ActivatedRoute, private router: Router) {}
 
@@ -16,10 +17,17 @@ isError: boolean = false;
     this.route.queryParamMap.subscribe(params => {
       this.isError = params.get('isError') === 'true';
       this.message = params.get('message') || '';
+      this.orderId = params.get('orderId');
     });
   }
 
   goToOrders() {
     this.router.navigate(['/orders']);
+  }
+
+  goToOrderComplete() {
+    if (this.orderId) {
+      this.router.navigate([`/orderComplete/${this.orderId}`]);
+    }
   }
 }
